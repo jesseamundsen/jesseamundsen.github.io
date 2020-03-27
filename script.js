7be3c72bb8d6e9dd2eedd87aeb677c272ac4f825
jesseamundsen <jesseamundsen@costquest.com>
2020-03-27 15:36:01 -0400

    Graphs multiple

--- script.js
+++ script.js
@@ -13,7 +13,11 @@ function init() {
                     iconSize: [iconsz, iconsz]
                 })
             }).on('click', function(e) {
-                display(data[i]["state"]);
+                display(data[i]["state"]
+                    ,response.find(x => x.state === data[i]["state"])["positive"]
+                    ,response.find(x => x.state === data[i]["state"])["totalTestResults"]
+                    ,response.find(x => x.state === data[i]["state"])["death"]
+                    );
             }).addTo(map);
         })
     });
@@ -30,23 +34,52 @@ function iconsize(count) {
     return 2+(2*i);
 }
 
-function display(state) {
+function display(state, cases, tests, deaths) {
+    $("#table tbody").html('').append('<tr><td>'+state+'</td><td>'+cases+'</td><td>'+tests+'</td><td>'+deaths+'</td></tr>');
     $.getJSON("https://covidtracking.com/api/states/daily?state=" + state, function(response) {
         var datax = [];
-        var datay = [];    
+        var datay_cases = [];
+        var datay_tests = [];
+        var datay_deaths = [];
         $.each(response, function(i) {
             datax.push(response[i]["date"].toString().slice(0,4)+"-"+response[i]["date"].toString().slice(4,6)+"-"+response[i]["date"].toString().slice(6,8));
-            datay.push(response[i][$("#measure").val()]);
+            datay_cases.push(response[i]["positive"]);
+            datay_tests.push(response[i]["totalTestResults"]);
+            datay_deaths.push(response[i]["death"]);
         });
-        var plot = document.getElementById('display');
-        Plotly.newPlot(plot
+
+        var plot_cases = document.getElementById('display_cases');
+        Plotly.newPlot(plot_cases
             ,[{
                  x: datax
-                ,y: datay
+                ,y: datay_cases
              }]
             ,{
                  margin:{t:0}
             }
         );
+
+        var plot_tests = document.getElementById('display_tests');
+        Plotly.newPlot(plot_tests
+            ,[{
+                 x: datax
+                ,y: datay_tests
+             }]
+            ,{
+                 margin:{t:0}
+            }
+        );
+
+        var plot_deaths = document.getElementById('display_deaths');
+        Plotly.newPlot(plot_deaths
+            ,[{
+                 x: datax
+                ,y: datay_deaths
+             }]
+            ,{
+                 margin:{t:0}
+            }
+        );
+
     });
 }
